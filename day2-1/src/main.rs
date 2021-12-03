a7ec4040595d3ecacf5809af550177b11c0e7f10
icefire <jackalstomper@users.noreply.github.com>
2021-12-02 21:26:15 -0500

    Golf day2

--- day2-1/src/main.rs
+++ day2-1/src/main.rs
@@ -1,47 +1,25 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
-struct Order {
-    direction: Direction,
-    amount: u32,
-}
-
-enum Direction {
-    Forward,
-    Down,
-    Up,
-}
-
 fn main() {
     let file = File::open("./input").unwrap();
-    let orders: Vec<Order> = io::BufReader::new(file)
-        .lines()
-        .filter_map(io::Result::ok)
-        .map(|line| {
-            let mut words = line.split(" ");
-            let dir = match words.next().unwrap() {
-                "forward" => Direction::Forward,
-                "down" => Direction::Down,
-                "up" => Direction::Up,
-                _ => panic!("Unsupported direction"),
-            };
-
-            Order {
-                direction: dir,
-                amount: words.next().unwrap().parse::<u32>().unwrap(),
-            }
-        })
-        .collect();
-
     let mut x = 0;
     let mut y = 0;
-    for order in orders.iter() {
-        match &order.direction {
-            Direction::Forward => x += order.amount,
-            Direction::Down => y += order.amount,
-            Direction::Up => y -= order.amount,
-        }
-    }
+    io::BufReader::new(file)
+        .lines()
+        .filter_map(io::Result::ok)
+        .for_each(|line| {
+            let mut words = line.split(' ');
+            let dir = words.next().unwrap();
+            let amount = words.next().unwrap().parse::<u32>().unwrap();
+
+            match dir {
+                "down" => y += amount,
+                "up" => y -= amount,
+                "forward" => x += amount,
+                _ => panic!("Unsupported direction"),
+            }
+        });
 
     println!("Final pos is {}", x * y);
 }
