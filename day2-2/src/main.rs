9a59746997cac8a604d12f725b65c77df2d9b96b
icefire <jackalstomper@users.noreply.github.com>
2021-12-02 21:13:21 -0500

    day2

---
+++ day2-2/src/main.rs
@@ -0,0 +1,51 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+struct Order {
+    direction: Direction,
+    amount: u32,
+}
+
+enum Direction {
+    Forward,
+    Down,
+    Up,
+}
+
+fn main() {
+    let file = File::open("./input").unwrap();
+    let orders: Vec<Order> = io::BufReader::new(file)
+        .lines()
+        .filter_map(io::Result::ok)
+        .map(|line| {
+            let mut words = line.split(" ");
+            let dir = match words.next().unwrap() {
+                "forward" => Direction::Forward,
+                "down" => Direction::Down,
+                "up" => Direction::Up,
+                _ => panic!("Unsupported direction"),
+            };
+
+            Order {
+                direction: dir,
+                amount: words.next().unwrap().parse::<u32>().unwrap(),
+            }
+        })
+        .collect();
+
+    let mut x = 0;
+    let mut y = 0;
+    let mut a = 0;
+    for order in orders.iter() {
+        match &order.direction {
+            Direction::Down => a += order.amount,
+            Direction::Up => a -= order.amount,
+            Direction::Forward => {
+                x += order.amount;
+                y += a * order.amount;
+            }
+        }
+    }
+
+    println!("Final pos is {}", x * y);
+}
